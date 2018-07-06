a327ead1e2bf919a939453ba078c95728849103f
root <egar.garcia@gmail.com>
2018-07-06 03:30:40 +0000

    Setting queue size of /image_color to 1, in order to ignore old images

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -45,7 +45,7 @@ class TLDetector(object):
         rely on the position of the light and the camera image to predict it.
         '''
         sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
-        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb, queue_size=20,
+        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb, queue_size=1,
                                 buff_size=100*1024*1024, tcp_nodelay=True)
 
         config_string = rospy.get_param("/traffic_light_config")
